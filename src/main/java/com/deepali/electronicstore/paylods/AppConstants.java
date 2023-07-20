d45453dbfb6b83678f314f1a66482542464dc02a
DeepaliMali <shewale.deeps@gmail.com>
2023-07-20 14:07:02 +0530

    AppConstants added.

--- src/main/java/com/deepali/electronicstore/paylods/AppConstants.java
+++ src/main/java/com/deepali/electronicstore/paylods/AppConstants.java
@@ -5,13 +5,15 @@ public class AppConstants {
    public static final String DELETE_USER="User Deleted Successfully";
    public static final String DELETE_PRODUCT="Product Deleted Successfully";
 
-//   public static final String PAGE_NUMBER="0";
-//   public static final String PAGE_SIZE="10";
-//    public static final String SORT_BY="name";
-//    public static final String SORT_DIR="asc";
-//
-//    public static final String SORT_BY_TITLE="title";
-//
+   public static final String DELETE_CATEGORY="Category Deleted Successfully";
+
+   public static final String PAGE_NUMBER="0";
+   public static final String PAGE_SIZE="10";
+   public static final String SORT_BY="name";
+   public static final String SORT_DIR="asc";
+
+    public static final String SORT_BY_TITLE="title";
+
 
 
 
